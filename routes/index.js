a71c43cf686b210a52b44f12eac95864b951fade
Rohitparjanya <77491863+rohitparjanya@users.noreply.github.com>
2021-07-23 08:28:23 +0530

    add contorller and routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const homecontroler = require('../controllers/hoomecontroler');
+
+router.get('/',homecontroler.home);
+
+module.exports = router;
